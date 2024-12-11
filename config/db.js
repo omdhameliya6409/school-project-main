b40062ddc0b62f188cf847838dde122e704b6587
OMDHAMELIYA6409 <OMDHAMELIYA0707@GMAIL.COM>
2024-12-11 14:53:05 +0530

    school project

--- config/db.js
+++ config/db.js
@@ -1,23 +1,18 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
 const dbconnect = async () => {
-    if (!process.env.CONNECTION_STRING) {
-        console.error("MongoDB URI is not defined in the environment variables.");
-        process.exit(1);
-    }
-
-    try {
-        console.log("Connecting to MongoDB with URI:", process.env.CONNECTION_STRING);
-
-        await mongoose.connect(process.env.CONNECTION_STRING, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        });
-        console.log("Database connected successfully");
-    } catch (error) {
-        console.error("Error connecting to the database:", error.message);
-        process.exit(1);  // Exit with failure code
-    }
+  try {
+    await mongoose.connect(
+      process.env.CONNECTION_STRING, // URI કનેક્શન સ્ટ્રિંગ
+      {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      }
+    );
+    console.log('✅ Connected to MongoDB successfully');
+  } catch (error) {
+    console.error('❌ Error connecting to the database:', error.message);
+  }
 };
 
 module.exports = dbconnect;
