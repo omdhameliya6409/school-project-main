b764a5aa432596e6f88a1bf5a42764a138d0a957
OMDHAMELIYA6409 <OMDHAMELIYA0707@GMAIL.COM>
2024-12-11 16:36:45 +0530

    school project

--- config/db.js
+++ config/db.js
@@ -2,10 +2,7 @@ const mongoose = require("mongoose");
 
 const dbconnect = async () => {
     try {
-        const connect = await mongoose.connect(process.env.CONNECTION_STRING, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        });
+        const connect = await mongoose.connect(process.env.CONNECTION_STRING);
         console.log("Database connected successfully");
     } catch (error) {
         console.error("Error connecting to the database:", error);
