8545cd148cbbffd999b14cfb9e80bc6a451cdc05
OMDHAMELIYA6409 <OMDHAMELIYA0707@GMAIL.COM>
2024-12-17 14:38:13 +0530

    school

--- models/Student.js
+++ models/Student.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const studentSchema = new mongoose.Schema({
   admissionNo: { type: String, required: true, unique: true },
   name: { type: String, required: true },
-  rollNo: { type: String, required: true },
+  rollNo: { type: Number, required: true },
   class: { type: String, required: true },
   fatherName: { type: String, required: true },
   dateOfBirth: { type: Date, required: true },
@@ -14,9 +14,9 @@ const studentSchema = new mongoose.Schema({
   deleted: Boolean,
   assignedTeacher: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
   section: { type: String, enum: ['A', 'B','C','D'], required: true },
-  house: { type: String, enum: ['Red', 'Blue', 'Green', 'Yellow'], required: true },
-  houseName: { type: String, required: true },
-  houseDescription: { type: String, required: true },
+  house: { type: String, enum: ['Red', 'Blue', 'Green', 'Yellow'] },
+  houseName: { type: String },
+  houseDescription: { type: String },
 });
 
 module.exports = mongoose.model("Student", studentSchema);
