17daabca5ab1d6ae0aa4bb8e6f2bde44014d8a1c
OMDHAMELIYA6409 <OMDHAMELIYA0707@GMAIL.COM>
2025-01-03 16:34:55 +0530

    school

--- routes/classTimetableRoutes.js
+++ routes/classTimetableRoutes.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const { addClassTimetable, getClassTimetable } = require('../controllers/classTimetableController');
-
+const authMiddleware = require('../middleware/authMiddleware');
 const router = express.Router();
 
 // Route to add a new class timetable entry (POST)
-router.post('/add', addClassTimetable);
+router.post('/add',authMiddleware(['principalAccess', 'teacherAccess']) , addClassTimetable);
 
 // Route to get the class timetable for a specific class (GET)
-router.get('/show', getClassTimetable);
+router.get('/show',authMiddleware(['principalAccess', 'teacherAccess']) , getClassTimetable);
 
 module.exports = router;
