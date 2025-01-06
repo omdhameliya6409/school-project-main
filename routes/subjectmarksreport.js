9a82e3819a02a075eed27f526a60c92d69abdca1
OMDHAMELIYA6409 <OMDHAMELIYA0707@GMAIL.COM>
2025-01-06 16:21:26 +0530

    school

--- routes/subjectmarksreport.js
+++ routes/subjectmarksreport.js
@@ -2,7 +2,6 @@ const express = require("express");
 const router = express.Router();
 const { createSubjectMarksReport, editSubjectMarksReport, deleteSubjectMarksReport, getAllSubjectMarksReports } = require("../controllers/subjectmarksreportController.js"); // Adjust the path
 
-// POST API to create a new subject marks report
 router.post("/add", createSubjectMarksReport);
 router.put("/edit/:id", editSubjectMarksReport);
 router.delete("/delete/:id", deleteSubjectMarksReport);
