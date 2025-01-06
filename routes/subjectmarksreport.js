310761fbcb8b095c0c8ebebb9754f821d8423b37
OMDHAMELIYA6409 <OMDHAMELIYA0707@GMAIL.COM>
2025-01-06 16:25:16 +0530

    school

--- routes/subjectmarksreport.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { createSubjectMarksReport, editSubjectMarksReport, deleteSubjectMarksReport, getAllSubjectMarksReports } = require("../controllers/subjectmarksreportController.js"); // Adjust the path
-
-router.post("/add", createSubjectMarksReport);
-router.put("/edit/:id", editSubjectMarksReport);
-router.delete("/delete/:id", deleteSubjectMarksReport);
-router.get("/list" , getAllSubjectMarksReports)
-module.exports = router;
