5987a3d6fcf6194e3dbad2f588efb63bfca106ee
OMDHAMELIYA6409 <OMDHAMELIYA0707@GMAIL.COM>
2025-01-06 16:03:12 +0530

    school

--- routes/teacherRoutes.js
+++ routes/teacherRoutes.js
@@ -13,5 +13,4 @@ const router = express.Router();
 // POST route to add a new teacher and user
 router.post('/admissions/add', authMiddleware(["principalAccess"]) ,addTeacher);
 router.get('/details' , authMiddleware(["principalAccess"]), getTeacherList);
-
 module.exports = router;
