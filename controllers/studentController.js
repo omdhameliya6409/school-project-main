220682c98ecf6dbd85751ea41c82b55c53da9eca
OMDHAMELIYA6409 <OMDHAMELIYA0707@GMAIL.COM>
2025-01-08 09:42:49 +0530

    profile

--- controllers/studentController.js
+++ controllers/studentController.js
@@ -1,11 +1,22 @@
 const Student = require('../models/Student');
-
-// Fetch all students
-exports.getAllStudents = async (req, res) => {
+const admissions = require('../models/Admission');
+// Get student profile by admissionNo
+exports.getStudentProfileByAdmissionNo = async (req, res) => {
   try {
-    const students = await Student.find();
-    res.status(200).json(students);
+      const { admissionNo } = req.query; // Get admissionNo from query parameters
+
+      if (!admissionNo) {
+          return res.status(400).json({ status: 400, message: 'Admission number is required to fetch student profile.' });
+      }
+
+      const studentProfile = await admissions.findOne({ admissionNo });
+
+      if (!studentProfile) {
+          return res.status(404).json({ status: 404, message: 'Student profile not found.' });
+      }
+
+      res.status(200).json({ status: 200, data: studentProfile });
   } catch (error) {
-    res.status(500).json({ message: 'Error fetching students' });
+      res.status(500).json({ status: 500, message: 'Error fetching student profile', error: error.message });
   }
 };
